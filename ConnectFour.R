9588ecdad4bf066123eb0d99bcbc6a68bee83420
John Rothen <rackjothen@gmail.com>
2020-11-06 21:04:01 -0500

    Updated Code for simple AI moves

--- ConnectFour.R
+++ ConnectFour.R
@@ -57,6 +57,17 @@ c4placepiece <- function(board,column, p){
   return(board)
 }
 
+aimove <- function(board){
+  #check board cols
+  temp <- rep(0,ncol(board))
+  p <- rep(0,ncol(board))
+  for(i in 1:ncol(board)){
+    temp[i] <- length(which(board[,i] ==0))
+  }
+  s <- sum(temp)
+  p <- temp/s
+  return(sample(1:7, 1, prob= p))
+}
 
 connectfour <- function(){
   #initialize board
@@ -64,21 +75,21 @@ connectfour <- function(){
   #collect player names
   p1<-readline(prompt= "Player 1 name:")
   p2<-readline(prompt= "Player 2 name:")
-  repeat{
-    #show board
-    prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6))
-    #Player 1 turn
-    turn <- p1
-    #intake move
-    p1move <- readline(prompt= "Player 1 move:")
-    #update board
-    board <- c4placepiece(board, p1move,1)
-    
+  if(p2 != "AI"){
+     repeat{
+      #show board
+      prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6), quote=FALSE)
+      #Player 1 turn
+      turn <- p1
+      #intake move
+      p1move <- readline(prompt= "Player 1 move:")
+      #update board
+      board <- c4placepiece(board, p1move,1)
     #check for a p1 win
     if(wincheck(board == 1)){
       break}
     #show new board
-    prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6))
+    prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6), quote=FALSE)
     #Player 2 turn
     turn <- p2
     #intake move
@@ -86,8 +97,76 @@ connectfour <- function(){
     #update board
     board <- c4placepiece(board, p2move,2)
     #board is shown here if win, or shown upon repeat!
+    if(wincheck(board == 1)){
+      break}
+     }
+  }else{
+    repeat{
+      #show board
+      prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6), quote=FALSE)
+      #Player 1 turn
+      turn <- p1
+      #intake move
+      p1move <- readline(prompt= "Player 1 move:")
+      #update board
+      board <- c4placepiece(board, p1move,1)
+      #check for a p1 win
+      if(wincheck(board == 1)){
+        break}
+      #show new board
+      #Player 2 turn
+      turn <- p2
+      #intake AI move
+      p2move <- aimove(board)
+      #update board
+      board <- c4placepiece(board, p2move,2)
+      #board is shown here if win, or shown upon repeat!
+      if(wincheck(board == 1)){
+        break}
+    }
   }
-  prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6))
+  prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6), quote=FALSE)
   cat(turn, "has won!")
 }
+
+#ai v ai, will add as option to normal func when both player names are AI
+
+aifight<-function(){
+  #initialize board
+  board <-matrix(0, nrow=6, ncol=7)
+  #collect player names
+  p1<-"AI"
+  p2<- "AI"
+  repeat{
+    #show board
+    prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6))
+    #Player 1 turn
+    turn <- "AI 1"
+    #intake move
+    p1move <- aimove(board)
+    #update board
+    board <- c4placepiece(board, p1move,1)
+    #check for a p1 win
+      if(wincheck(board == 1)){
+      break}
+    #show new board
+    #Player 2 turn
+    turn <- "AI 2"
+    #intake AI move
+    p2move <- aimove(board)
+    #update board
+    board <- c4placepiece(board, p2move,2)
+    #board is shown here if win, or shown upon repeat!
+    if(wincheck(board == 1)){
+      break}
+    rl<-readline(prompt= "Continue? : ")
+    if(rl == "Y"){
+    }else(break)
+  }
+prmatrix(board, collab = c("c1","C2","C3","C4","C5","C6","C7"), rowlab = rep("",6))
+cat(turn, "has won!")
+}
+
+#library(gt)
+#gt makes nicer tables, might be worth switching
 
